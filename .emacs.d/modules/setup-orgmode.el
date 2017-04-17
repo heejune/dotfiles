f99477eb8dd0a741166b94224659f0e590d394f1
Heejune Kim <heejune@gmail.com>
2017-04-17 10:43:25 +0900

    Fix org capture note default directory for non-linux machine

--- .emacs.d/modules/setup-orgmode.el
+++ .emacs.d/modules/setup-orgmode.el
@@ -117,7 +117,7 @@
                   ("DONE" ("WAITING") ("CANCELLED") ("HOLD")))))
 
     (setq org-directory 'org-dir)
-    (setq org-default-notes-file "~/Dropbox/private/orgs/refile.org")
+    (setq org-default-notes-file (expand-file-name "refile.org" org-dir))
 
     ;; I use C-c c to start capture mode
     (global-set-key (kbd "C-c c") 'org-capture)
@@ -127,21 +127,21 @@
 
     ;; Capture templates for: TODO tasks, Notes, appointments, phone calls, meetings, and org-protocol
     (setq org-capture-templates
-          (quote (("t" "todo" entry (file "~/Dropbox/private/orgs/refile.org")
+          (quote (("t" "todo" entry (file org-default-notes-file)
                    "* TODO %?  :REFILE:\n%U\n%a\n" :clock-in t :clock-resume t)
-                  ("r" "respond" entry (file "~/Dropbox/private/orgs/refile.org")
+                  ("r" "respond" entry (file org-default-notes-file)
                    "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
-                  ("n" "note" entry (file "~/Dropbox/private/orgs/refile.org")
+                  ("n" "note" entry (file org-default-notes-file)
                    "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
-                  ("j" "Journal" entry (file+datetree "~/Dropbox/private/orgs/diary.org")
+                  ("j" "Journal" entry (file+datetree org-default-notes-file)
                    "* %?\n%U\n" :clock-in t :clock-resume t)
-                  ("w" "org-protocol" entry (file "~/Dropbox/private/orgs/refile.org")
+                  ("w" "org-protocol" entry (file org-default-notes-file)
                    "* TODO Review %c\n%U\n" :immediate-finish t)
-                  ("m" "Meeting" entry (file "~/Dropbox/private/orgs/refile.org")
+                  ("m" "Meeting" entry (file org-default-notes-file)
                    "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
-                  ("p" "Phone call" entry (file "~/Dropbox/private/orgs/refile.org")
+                  ("p" "Phone call" entry (file org-default-notes-file)
                    "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
-                  ("h" "Habit" entry (file "~/Dropbox/private/orgs/refile.org")
+                  ("h" "Habit" entry (file org-default-notes-file)
                    "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
 
     (add-to-list 'org-capture-templates '("b" "Buy list" entry (file org-default-notes-file)
